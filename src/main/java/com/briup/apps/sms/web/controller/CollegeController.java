a3dbcb05747f29806d1e84248ee324ba7df2cbf9
杨海龙 <1723043824@qq.com>
2019-07-11 21:01:55 +0800

    【杨海龙】更新

---
+++ src/main/java/com/briup/apps/sms/web/controller/CollegeController.java
@@ -0,0 +1,48 @@
+package com.briup.apps.sms.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.College;
+import com.briup.apps.sms.service.CollegeService;
+
+@RestController
+@RequestMapping("/college")
+public class CollegeController {
+	@Autowired
+	private CollegeService collegeService;
+	@PostMapping("saveOrUpdate")
+	public String saveOrUpdate(College college)
+	{
+		try {
+			collegeService.saveOrUpdate(college);
+			return "更新成功";
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			return e.getLocalizedMessage();
+		}
+	}
+	// http://localhost:8080/school/selectAll
+	@GetMapping("selectAll")
+	public List<College> selectAll(){
+		return collegeService.selectAll();
+	}
+	// http://localhost:8080/school/deleteById?id=3
+		@GetMapping("deleteById")
+		public String deleteById(long id) {
+			try {
+				collegeService.deleteById(id);
+				return "删除成功";
+			} catch (Exception e) {
+				// 打印异常信息，返回异常信息
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
+}
