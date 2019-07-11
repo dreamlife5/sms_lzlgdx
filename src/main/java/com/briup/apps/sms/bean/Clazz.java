57a5e0283f9d681a9b57bd115485a0c9e0dcef38
杨雷雷 <2433475144@qq.com>
2019-07-11 23:19:47 +0800

    【杨雷雷】更新

---
+++ src/main/java/com/briup/apps/sms/bean/Clazz.java
@@ -0,0 +1,32 @@
+package com.briup.apps.sms.bean;
+
+public class Clazz {
+	private Long id;
+	private String name;
+	private Long begin_time;
+	private Long college_id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getBegin_time() {
+		return begin_time;
+	}
+	public void setBegin_time(Long begin_time) {
+		this.begin_time = begin_time;
+	}
+	public Long getCollege_id() {
+		return college_id;
+	}
+	public void setCollege_id(Long college_id) {
+		this.college_id = college_id;
+	}
+}
