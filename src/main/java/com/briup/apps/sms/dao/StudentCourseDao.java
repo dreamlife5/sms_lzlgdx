b3bd10277bcd5e566bf31e9bb496ab95aade9324
王晶铖 <751590264@qq.com>
2019-07-12 00:29:24 +0800

    【王晶铖】更新

---
+++ src/main/java/com/briup/apps/sms/dao/StudentCourseDao.java
@@ -0,0 +1,16 @@
+package com.briup.apps.sms.dao;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.StudentCourse;
+
+public interface StudentCourseDao {
+	//查询所有
+    List<StudentCourse> selectAll();
+    //插入
+    void insert(StudentCourse studentCourse);
+    //更新
+    void update(StudentCourse studentCourse);
+    // 通过ID删除
+ 	void deleteById(long id);
+}
---
+++ src/main/java/com/briup/apps/sms/service/impl/StudentCourseServiceImpl.java
@@ -0,0 +1,38 @@
+package com.briup.apps.sms.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.StudentCourse;
+import com.briup.apps.sms.dao.StudentCourseDao;
+import com.briup.apps.sms.service.StudentCourseService;
+/**
+ *校园业务逻辑处理的实现类 
+ **/
+@Service
+public class StudentCourseServiceImpl implements StudentCourseService{
+    
+	@Resource
+	private StudentCourseDao studentCourseDao;
+	@Override
+	public List<StudentCourse> selectAll(){
+	return studentCourseDao.selectAll();
+	}
+	@Override
+	public void saveOrUpdate(StudentCourse studentCourse) throws Exception{
+		if(studentCourse.getId() == null) {
+			studentCourseDao.insert(studentCourse);
+		}else {
+			studentCourseDao.update(studentCourse);
+		}
+	}
+	@Override
+	public void deleteById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		studentCourseDao.deleteById(id);
+	}
+
+}
