d7c053f81185656f15a4cd96c97799cf9b9344b1
张成 <zhangcheng1996603@qq.com>
2019-07-12 10:02:26 +0800

    [张成]提交代码

--- src/main/java/com/briup/apps/sms/web/controller/CourseController.java
+++ src/main/java/com/briup/apps/sms/web/controller/CourseController.java
@@ -13,39 +13,36 @@ import com.briup.apps.sms.service.CourseService;
 
 @RestController
 @RequestMapping("/course")
-public class CourseController { 
-	
+public class CourseController {
 	@Autowired
 	private CourseService courseService;
 	@PostMapping("saveOrUpdate")
-	public String saveOrUpdate(Course course) {
+	public String saveOrUpdate(Course course)
+	{
 		try {
 			courseService.saveOrUpdate(course);
-			return "保存或更新成功";
-		} catch (Exception e) {
-			// TODO: handle exception
+			return "更新成功";
+		}catch(Exception e)
+		{
 			e.printStackTrace();
-			return e.getMessage();
+			return e.getLocalizedMessage();
 		}
 	}
-	// http://localhost:8080/Course/selectAll
+	// http://localhost:8080/school/selectAll
 	@GetMapping("selectAll")
 	public List<Course> selectAll(){
 		return courseService.selectAll();
 	}
-	 
-	// http://localhost:8080/course/deleteById?id=3
-	@GetMapping("deleteById")
-	public String deleteById(long id) {
-		try {
-			courseService.deleteById(id);
-			return "删除成功";
-		} catch (Exception e) {
-			// 打印异常信息，返回异常信息
-			e.printStackTrace();
-			return e.getMessage();
+	// http://localhost:8080/school/deleteById?id=3
+		@GetMapping("deleteById")
+		public String deleteById(long id) {
+			try {
+				courseService.deleteById(id);
+				return "删除成功";
+			} catch (Exception e) {
+				// 打印异常信息，返回异常信息
+				e.printStackTrace();
+				return e.getMessage();
+			}
 		}
-		
-	}
-	
 }
